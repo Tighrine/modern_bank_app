9f8d6ff8323043c4b0f5801fb3ed6c1b8174ffff
Aghiles TIGHRINE <aghiles.tighrine2@gmail.com>
2022-08-19 12:28:27 +0200

    update robot image style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,18 +40,18 @@ const Hero = () => {
           We examine annual percentage rates, annual fees.
         </p>
       </div>
-
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted />
-      </div>
-
       
-      <div>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} className="w-auto h-auto z-[5]" />
         <div className="absolute w-[40%] h-[35%] top-0 pink__gradient z-0 " />
         <div className="absolute w-[80%] h-[80%] bottom-40 white__gradient z-[1] " />
         <div className="absolute w-[50%] h-[50%] right-20 bottom-20 blue__gradient z-[0]  " />
       </div>
+      
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+
     </section>
   )
 }
