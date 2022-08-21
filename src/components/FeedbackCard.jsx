b9e86d2546d1a11c9735e779087842029ee380b8
Aghiles TIGHRINE <aghiles.tighrine2@gmail.com>
2022-08-21 10:42:35 +0200

    fix log warning

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -4,7 +4,7 @@ const FeedbackCard = ({ id, content, name, title, img }) => {
   return (
     <div className='flex flex-col rounded-3xl feedback-card px-10 py-12 
     justify-between items-start max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5' >
-      <img src={quotes} alt="quotes" className="w-[42.6px] h-[27.6px] " />
+      <img src={quotes} alt="quotes" className="w-[42.6px] h-[27.6px]" />
       
       <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
         {content}
